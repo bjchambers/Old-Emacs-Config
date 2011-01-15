86728df9526eb057490c5cc70a0f66a24d3f211e
Ben Chambers <bjchambers@gmail.com>
2011-01-14 23:37:06 -0800

    Adding support for haskell.

---
+++ haskell-init.el
@@ -0,0 +1,5 @@
+(load (concat emacs-conf-dir "haskell-mode-2.8.0/haskell-site-file"))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
