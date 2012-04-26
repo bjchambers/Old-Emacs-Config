6dc23792d1be597fa48b85d5fe4f38364eb26804
Ben Chambers <bjchambers@gmail.com>
2012-04-25 23:06:43 -0700

    Adding flymake.

--- python-init.el
+++ python-init.el
@@ -3,6 +3,15 @@
 (setq py-install-directory (concat emacs-conf-dir "python-mode.el-6.0.6"))
 (require 'python-mode)
 
-;; Setup pony mode for django
-(add-to-list 'load-path (concat emacs-conf-dir "pony-mode/src"))
-(require 'pony-mode)
+;; Setup pyflakes
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'python-mode-hook 'flymake-mode)
